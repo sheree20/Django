2bbecdfed6e71b13814a84db4dd339c1ce35ef38
sheree20 <shereemaelapore0@gmail.com>
2026-02-03 19:41:25 +0800

    Add files via upload

---
+++ views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def features(request):
+    return render(request, 'features.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
